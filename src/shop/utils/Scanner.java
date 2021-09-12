c8bc4d93cca91cf504fddf56bfeba33806d3b5d7
Ehsan Abbas Nejad <ehsan.absnjd@gmail.com>
2021-09-12 11:48:53 +0430

    update scanner class

--- src/shop/utils/Scanner.java
+++ src/shop/utils/Scanner.java
@@ -1,2 +1,47 @@
-package shop.utils;public class Scanner {
+package shop.utils;
+
+
+public class Scanner {
+    java.util.Scanner sc = new java.util.Scanner(System.in );
+    public int getInt(){
+        int a;
+        while(true){
+            try{
+                String st = sc.next();
+                a = Integer.valueOf(st);
+                break;
+            }catch (Exception e){
+                System.out.println("invalid input, try again");
+            }
+        }
+        return a;
+    }
+    public double getDouble(){
+        double a;
+        while(true){
+            try{
+                String st = sc.next();
+                a = Double.valueOf(st);
+                break;
+            }catch (Exception e){
+                System.out.println("invalid input, try again");
+            }
+        }
+        return a;
+    }
+    public String getString(){
+        return sc.next();
+    }
+    public String getEmail(){
+        String st ;
+        while(true) {
+            st = sc.next();
+            if(st.indexOf("@")>0 &&
+                    st.lastIndexOf(".") >st.indexOf("@")+1 &&
+                    st.lastIndexOf(".") <st.length()-1 )
+                break;
+            System.out.println("invalid email, try again");
+        }
+        return st;
+    }
 }
